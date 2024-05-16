a525bca3c9046cfb7fd9e1b9187f85356dfd6163
Akshay Gulhane <argulhane10246@gmail.com>
2024-05-16 23:45:37 +0530

    Commit1605241

---
+++ src/redux/trainerSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const trainerSlice = createSlice({
+    name : 'trainer',
+    initialState :{
+        list:[{
+            name : 'Tony',
+            mobileNumber : '123123',
+            address : "Test address",
+            salary : 20000,
+            clients : [
+                "Tony","Mony"
+            ],
+            inTime: '20-10',
+            outTime : "21-10"
+        }]
+    },
+    reducers:{
+        addTrainer : (state,action) => {
+            state.list.push(action.payload);
+        }
+    }
+})
+
+export const {addTrainer} = createSlice.actions;
+export const selectTrainer = (state) => state.trainer.list;
+export default trainerSlice.reducer;
